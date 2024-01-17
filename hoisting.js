12811eb5e44803eb1c8453f5385b4b1883844b67
Estefany Aguilar <tefy.1@live.com>
2024-01-17 15:57:59 -0500

    Add files via upload

---
+++ hoisting.js
@@ -0,0 +1,17 @@
+console.log(getTotal(10, 5))
+//console.log(userName)
+//console.log(userDetails)
+console.log(currentCity)
+
+function getTotal (a, b) {
+    return a * b
+}
+
+const userName = "Jhon"
+
+let userDetails = {
+    age: 30,
+    country: "USA"
+}
+
+var currentCity = "New York"
